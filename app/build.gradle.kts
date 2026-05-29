3599b9d07693e515c21a73882ffc8289683f0ccf
jinglekang <jinglekang@live.cn>
2026-05-29 10:31:26 +0800

    chore: release 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         // noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
